31a58d1d551a2e3ab5430a9ea0579f1c4cae97fc
Jochen Smolka <smolkajochen@gmail.com>
2022-01-26 12:55:52 +0100

    updating maxfig

--- methods/general/maxfig.m
+++ methods/general/maxfig.m
@@ -1,13 +1,23 @@
-function maxfig(handle)
+function maxfig(fh)
 
-if nargin<1
-    handle = gcf;
+%% inputs
+if nargin<1, fh = gcf; end
+
+if verLessThan('matlab', '9.4') 
+    %% for matlab version before 2018a, use Java
+
+    % deactivate warnings
+    s = warning('off', 'MATLAB:HandleGraphics:ObsoletedProperty:JavaFrame');
+    warning('off', 'MATLAB:ui:javaframe:PropertyToBeRemoved');
+
+    % maximise figure
+    pause(0.01); % makes this work even if the figure has only just been opened 
+    jf = get(fh, 'JavaFrame');
+    set(jf,'Maximized',1);
+
+    % reset warnings
+    warning(s)
+else
+    %% from 2018a onwards, use new figure property
+    fh.WindowState = 'maximized';
 end
-handle.WindowState = 'maximized';
-
-%% Legacy Code:
-% warning('off','MATLAB:HandleGraphics:ObsoletedProperty:JavaFrame');
-% pause(0.01); % makes this work even if the figure has only just been opened 
-% jf = get(handle,'JavaFrame');
-% set(jf,'Maximized',1);
-% % jf.getFigurePanelContainer.getComponent(0).getTopLevelAncestor.setMaximized(1)
